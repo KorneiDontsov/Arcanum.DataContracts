a1c94b6d20ad2514a24a55b8aed10536d9dab020
Kornei Dontsov <KorneiDontsov@gmail.com>
2019-07-23 19:43:03 +0300

    Implement 'DataTypeInfo'
    Metadata of discriminated unions and their cases.

---
+++ Source/Arcanum.DataContracts/api/implementations/DiscriminatedUnionCaseInfo.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT. See LICENSE in the project root for license information.
+
+using System;
+using System.Reflection;
+
+namespace Arcanum.DataContracts
+{
+	public sealed class DiscriminatedUnionCaseInfo
+	{
+		public DataTypeInfo typeInfo { get; }
+
+		public DiscriminatedUnionInfo declaringUnionInfo { get; }
+
+		public String name { get; }
+
+		public Type type => typeInfo.type;
+
+		internal DiscriminatedUnionCaseInfo (DataTypeInfo typeInfo, DiscriminatedUnionInfo declaringUnionInfo)
+		{
+			this.typeInfo = typeInfo;
+			this.declaringUnionInfo = declaringUnionInfo;
+
+			name = (
+				typeInfo.type.GetCustomAttribute(typeof(IDataCaseAttribute), inherit: true)
+				as IDataCaseAttribute
+			)
+			?.name
+			?? typeInfo.type.Name;
+		}
+	}
+}
---
+++ Source/Arcanum.DataContracts/api/implementations/DiscriminatedUnionInfo.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+
+namespace Arcanum.DataContracts
+{
+	public sealed class DiscriminatedUnionInfo
+	{
+		public DataTypeInfo typeInfo { get; }
+
+		public IImmutableList<DiscriminatedUnionCaseInfo> caseInfos { get; }
+
+		public IImmutableDictionary<Type, DiscriminatedUnionCaseInfo> caseInfosByTypes { get; }
+
+		public IImmutableDictionary<String, DiscriminatedUnionCaseInfo> caseInfosByNames { get; }
+
+		public Type type => typeInfo.type;
+
+		internal DiscriminatedUnionInfo (
+			DataTypeInfo typeInfo,
+			Func<DiscriminatedUnionInfo, IImmutableList<DiscriminatedUnionCaseInfo>> caseInfoListConstructor
+		)
+		{
+			this.typeInfo = typeInfo;
+			caseInfos = caseInfoListConstructor(this);
+			caseInfosByTypes = caseInfos.ToImmutableDictionary(i => i.type);
+			caseInfosByNames = caseInfos.ToImmutableDictionary(i => i.name);
+		}
+	}
+}
