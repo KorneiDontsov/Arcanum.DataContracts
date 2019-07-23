611d16cbc12b27d25dc9403167dcac6b93326fed
Kornei Dontsov <KorneiDontsov@gmail.com>
2019-07-24 00:03:23 +0300

    Test 'DataTypeInfoStorage'

---
+++ Source/Tests.Arcanum.DataContracts/TestDataTypeInfoStorage.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT. See LICENSE in the project root for license information.
+
+using Arcanum.DataContracts;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Tests.Arcanum.DataContracts
+{
+	public abstract class TestDataTypeInfoStorage
+	{
+		public abstract class Base
+		{
+			private sealed class SomeData { }
+
+			protected IDataTypeInfoStorage storage { get; }
+
+			protected Base (IDataTypeInfoStorage storage)
+			{
+				this.storage = storage;
+			}
+
+			[Fact]
+			public void DoesntGiveNullInsteadInfo ()
+			{
+				var someDataTypeInfo = storage.Get(typeof(SomeData));
+
+				_ = someDataTypeInfo.Should().NotBeNull();
+			}
+
+			[Fact]
+			public void DoCache ()
+			{
+				var someDataTypeInfo = storage.Get(typeof(SomeData));
+				var sameDataTypeInfo = storage.Get(typeof(SomeData));
+
+				_ = someDataTypeInfo.Should().BeSameAs(sameDataTypeInfo);
+			}
+		}
+
+		public sealed class Shared : Base
+		{
+			public Shared () : base(DataTypeInfoStorage.shared) { }
+		}
+	}
+}
